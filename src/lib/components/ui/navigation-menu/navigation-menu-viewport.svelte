6ce8bb27060fc7ce64b2accb51db1380eb8ee8f6
jasonwill111 <jasonwill111@users.noreply.github.com>
2026-09-02 17:21:38 +0800

    fix: navigation dropdown viewport positioning and z-index

--- src/lib/components/ui/navigation-menu/navigation-menu-viewport.svelte
+++ src/lib/components/ui/navigation-menu/navigation-menu-viewport.svelte
@@ -9,7 +9,7 @@
 	}: NavigationMenuPrimitive.ViewportProps = $props();
 </script>
 
-	<div class={cn('absolute left-1/2 top-full isolate z-50 flex -translate-x-1/2 justify-center')}>
+	<div class={cn('absolute left-0 top-full isolate z-[100] flex justify-center')}>
 	<NavigationMenuPrimitive.Viewport
 		bind:ref
 		data-slot="navigation-menu-viewport"
